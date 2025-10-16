011d65c8935411bd57451336f73c273e8f0094d8
Zahid Hasan Milu <minions.milu889@gmail.com>
2025-10-16 14:08:38 +0600

    ApiVIEW, MixinView, GenegicView

--- app_home_api/serializers.py
+++ app_home_api/serializers.py
@@ -7,8 +7,47 @@ class CategorySerializer(serializers.ModelSerializer):
         fields = ['id', 'title', 'slug']
         read_only_fields = ['id', 'slug']
         
-class TagSerializer(serializers.ModelSerializer):
+# class TagSerializer(serializers.ModelSerializer):
+#     class Meta:
+#         model = Tag
+#         fields = ['id', 'title', 'slug']
+#         read_only_fields = ['id', 'slug']
+
+class TagSerializer(serializers.Serializer):
+    id = serializers.IntegerField(read_only=True)
+    title = serializers.CharField(max_length=50)
+    slug = serializers.CharField(read_only=True)
+
+    #create() → new Category create করে
+    def create(self, validated_data):
+        return Tag.objects.create(**validated_data)
+    
+    def update(self, instance, validated_data):
+        instance.title = validated_data.get('title', instance.title)
+        instance.slug = validated_data.get('slug', instance.slug)
+        instance.save()
+        return instance
+
+#-------------------------------------------------------------------------------
+#----------------------BLOG SERIALIZER------------------------------------------------
+#-------------------------------------------------------------------------------
+
+class BlogSerializer(serializers.ModelSerializer):
+    category = serializers.StringRelatedField()   # Category model-এর __str__() return হবে
+    author = serializers.StringRelatedField()     # Author model-এর __str__() return হবে
+    tags = serializers.StringRelatedField(many=True)  # প্রতিটা tag-এর নাম দেখাবে
     class Meta:
-        model = Tag
-        fields = ['id', 'title', 'slug']
-        read_only_fields = ['id', 'slug']
+        model = Blog
+        fields = ['id', 'title', 'slug', 'content', 'author', 'category', 'tags', 'created_date', 'created_date']
+        read_only_fields = ['id', 'slug', 'created_date', 'created_date']
+        
+    def validate_title(self, value):
+        if len(value) < 5:
+            raise serializers.ValidationError("Title must be at least 5 characters long.")
+        return value
+    
+    def validate_content(self, value):
+        if len(value) < 20:
+            raise serializers.ValidationError("Content must be at least 20 characters long.")
+        return value
+    
--- app_home_api/urls.py
+++ app_home_api/urls.py
@@ -1,6 +1,17 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('category/', views.category, name='categories_api'),  # List and create categories  
+    # path('category/', views.category, name='categories_api'),  # List and create categories  
+    # path('tag/', views.tag, name='tags_api'),  # List and create tags
 
-]
+    # path('category/', views.CategoryListCreateMixinView.as_view(), name='categories_api'),  # List and create categories
+    # path('category/<int:id>/', views.CategoryRetriveUpdateDeleteView.as_view(), name='category_detail_api'),  # Retrieve, update, delete a category
+    
+    path('category/', views.CategoryListCreateAPIView.as_view(), name='categories_api'),  # List and create categories
+    path('category/<int:id>/', views.CategoryRetrieveUpdateDestroyAPIView.as_view(), name='category_detail_api'),  # Retrieve, update, delete a category
+    
+    path('tag/', views.TagListCreateAPIView.as_view(), name='tags_api'),  # List and create tags
+    path('tag/<int:id>/', views.TagRetrieveUpdateDestroyAPIView.as_view(), name='tag_detail_api'),  # Retrieve, update, delete a tag
+    
+    path('blog/', views.BlogListCreateAPIView.as_view(), name='blogs_api'),  # List and create blogs
+]   
